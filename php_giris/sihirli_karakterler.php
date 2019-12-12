a816a3acdce492da764e8f205aa2b6ff15d93083
AlihanBayrak <alob43bayram@gmail.com>
2019-12-12 14:59:12 +0300

    magic characters

---
+++ php_giris/sihirli_karakterler.php
@@ -0,0 +1,16 @@
+<?php
+    /*
+        Sihirli Karakter
+        \t -->tab 
+        \n -->newline
+        \\ -->çakışmaması için
+        \$
+        \'
+        \"
+    */
+
+    $test = "Alihan\t\t\t\nBayrak  \\test\ ";
+    echo $test;
+    $ad = "Alihan";
+    echo "\$ad değişkeni $ad değerine denktir";
+?>
