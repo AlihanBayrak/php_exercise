91694226a298eda0919ee41eff1a0d13b896f111
AlihanBayrak <alob43bayram@gmail.com>
2019-12-12 10:12:01 +0300

    basic

---
+++ php_giris/degiskenler.php
@@ -0,0 +1,17 @@
+<?php
+
+    /*
+        Değişkenler;
+            $ işareti ile tanımlanır
+            Harf veya _ katrakteri ile başlarlar
+            Sayı ile başlayamazlar
+            Türkçe karakter içerebilir(Önerilmez)
+            Büyük-küçük harfe duyarlıdırlar.
+    */
+
+    $alihan = "Alihan Bayrak";
+    $mySite = "http://bil.184471049.wordpress.com";
+    echo $mySite;
+    //echo $alihan;
+
+?>
