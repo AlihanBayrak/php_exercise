8885c1dbef0ed9b364e822140cbcd5857f35f364
AlihanBayrak <alob43bayram@gmail.com>
2019-12-16 14:13:05 +0300

    functions

--- pho_fonksiyonlar/fonksiyon.php
+++ pho_fonksiyonlar/fonksiyon.php
@@ -1,5 +1,37 @@
 <?php
+    function Test(){
+        return "Merhaba Dünya";
+    }
+    $a = Test();
+    echo $a;
+    //Merhaba Dünya
 
+    function topla($sayi1, $sayi2){
+        return ($sayi1 + $sayi2);
+    }
+    $toplam = topla(6,15);
+    echo $toplam;
+    // 21
+    function topl($sayi3, $sayi4 = 10){
+        return ($sayi3 + $sayi4);
+    }
+    $topla = topl(6);
+    echo $topla;
+    // 16 Çünkü vasayılan değer 10 atadık sayi4'e
+
+    $ad = "Alihan";
+
+    /*
+        global
+        $GLOBALS
+    */
+    function adSoyad($soyad){
+        //global $ad;
+        return $GLOBALS['ad'] . " " . $soyad;
+    }
+    echo adSoyad('Bayrak');
+    //Alihan Bayrak
+    
     $yazi = "Alihan Bayrak Alihan Bayrak";
 
     function kisalt($str, $limit = 9)
