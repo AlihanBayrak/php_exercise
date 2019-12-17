04379a0b9a822ce5bb38c21692b768d45533880c
AlihanBayrak <alob43bayram@gmail.com>
2019-12-17 15:28:23 +0300

    array functions

---
+++ php_dizi_fonksiyonlar/dizi_fonk_1.php
@@ -0,0 +1,18 @@
+<?php
+/*
+    print_r()
+    var_dump()
+    explode()
+    implode()
+    count()
+    is_array()
+    shuffle()
+    array_combine()
+    array_count_values()
+    array_flip()
+    array_key_exists()
+*/
+
+
+
+?>
