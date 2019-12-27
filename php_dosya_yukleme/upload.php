eb79125eb96eee0ba6d7660c07f4f75d632eca08
AlihanBayrak <alob43bayram@gmail.com>
2019-12-27 10:20:19 +0300

    folder upload

---
+++ php_dosya_yukleme/upload.php
@@ -0,0 +1,5 @@
+<form action="sonuc.php" method="post" enctype="multipart/form-data">
+    Dosya Secin: <br>
+    <input type="file" name="doya"> <br>
+    <button type="submit">Yukle</button>
+</form>
